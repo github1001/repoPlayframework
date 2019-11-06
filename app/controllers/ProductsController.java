d5e1838d0b6647e18bd9e4dc6289dc1cc64b4aeb
Logan <github1001@github.com>
2019-11-07 01:45:30 +0800

    Added new products controller, routes and removed target folder

---
+++ app/controllers/ProductsController.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ProductsController extends Controller {
+
+      //for all products
+      public Result index(){
+          return TODO();
+      }
+
+      //to create our products
+    public Result create(){
+        return TODO();
+    }
+
+    //To sav products
+    public Result save(){
+          return TODO();
+    }
+
+    public Result edit(Integer id){
+          return TODO();
+    }
+    public Result update(){
+        return TODO();
+    }
+
+    public Result destruct(Integer id){
+        return TODO();
+    }
+
+    //for product details
+    public Result show(Integer id){
+        return TODO();
+    }
+
+
+}
